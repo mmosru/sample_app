75ece506b6a0de5ba41bb82937b5c39832e0549d
Michael OConnor <oconnor@my.ccsu.edu>
2016-11-16 18:06:31 +0000

    changed landing msg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "it works."
+    render html: "sample_app works."
   end
 end
